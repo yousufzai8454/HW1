1b8a910faf41a651a674af010fa2a8585618d2c6
yousufzai8454 <yousufzai8454@gmail.com>
2022-05-16 13:53:54 -0400

    the updated commit

--- src/com/company/main.java
+++ src/com/company/main.java
@@ -2,9 +2,10 @@ package com.company;
 
 public class main {
     public static void main(String[] args) {
-
+        //For clone project
         for (int i =0; i <=100; i++){
             System.out.println(i);
         }
+        System.out.println("I am working on a cloning project. ");
     }
 }
